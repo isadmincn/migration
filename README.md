78e642917250866daa67a8e4ccbdca6967d486b3
肖庆平 <xiaoqingping@qq.com>
2020-10-23 22:47:51 +0800

    配置注释

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ```
 composer require isadmin/migration
 ```
+
+安装完毕之后，在项目config目录中会出现一个migration.php，该文件为该扩展的配置文件
--- config/migration.php
+++ config/migration.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
+    // 自定义的Migration类
     'migration_base_class' => isadmin\migration\schema\Migration::class,
+    // 自定义的seed类
     'seed_base_class' => isadmin\migration\schema\Seed::class,
+    // 适配器映射
     'adapter_mapping' => [],
+    // 生成路径
     'paths' => [
         'migrations' => [
             'database/migrations',
@@ -12,8 +16,10 @@ return [
             'database/seeds',
         ],
     ],
+    // migration环境配置
+    // 该处只定义日志表，环境配置复用thinkphp的数据库配置
     'environments' => [
-        'default_migration_table' => '_phinxlog',
+        'default_migration_table' => 'migration_log',
     ],
     'version_order' => 'creation'
 ];
